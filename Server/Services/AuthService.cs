06f256d831cebf92ec0083b40dd2765b14a2c134
bigpigmoon <matvey.novikov02@gmail.com>
2023-11-25 01:03:19 +0700

    feat: add auth service

---
+++ Server/Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace Server.Services;
+
+public class AuthService : Auth.AuthBase
+{
+    public override Task<TokensResponse> Login(LoginRequest request, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<TokensResponse> Register(RegisterRequest request, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
